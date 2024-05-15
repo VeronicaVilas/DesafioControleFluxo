9f4853049ebbfcddf33085c395e3ea75767108ba
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-05-15 13:12:47 -0300

    feat: Adiciona impressão de exceção ao sistema

--- src/Contador.java
+++ src/Contador.java
@@ -12,8 +12,24 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = scanner.nextInt();
 
+        try {
+
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+
+            System.out.println("Erro: Para seguir com a aplicação, o segundo parâmetro deve ser maior que o primeiro!");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
         for(int contagem = parametroUm; contagem <= parametroDois; contagem++) {
             System.out.println("Imprimindo o número " + contagem);
         }
-    }
+	}
 }
